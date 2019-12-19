4980cc8ce46c84bdfc4ef956668376360e8f11cf
Vlad-Ber <vladik@live.se>
2019-12-19 13:46:24 +0100

    tests forts

--- proj/test.c
+++ proj/test.c
@@ -5,10 +5,13 @@
 #include "refmem.c"
 #include "iterator.h"
 
+typedef struct cell1 cell1_t;
+typedef struct cell2 cell2_t;
+
 
 struct cell1
 {
-  struct cell *cell;
+  struct cell1 *cell;
 
   char *string;
   int k;
@@ -31,20 +34,118 @@ void cell_destructor2(obj *c){release(((struct cell2 *) c)->cell);}
 
 void test_retain()
 {
-  struct cell1 *c1 = allocate(sizeof(struct cell1), cell_destructor1);
-  struct cell2 *c2 = allocate(sizeof(struct cell2), cell_destructor2);
+  cell1_t *c1 = allocate(sizeof(cell1_t), cell_destructor1);
+  cell2_t *c2 = allocate(sizeof(cell2_t), cell_destructor2);
   objectInfo_t *c1_Info = ((obj *) c1 - sizeof(objectInfo_t));
   objectInfo_t *c2_Info = ((obj *) c2 - sizeof(objectInfo_t));
 
+
+  CU_ASSERT_TRUE(c1_Info->rf == 0);
   retain(c1);
   CU_ASSERT_TRUE(c1_Info->rf == 1);
   CU_ASSERT_TRUE(c2_Info->rf == 0);
   retain(c2);
+  CU_ASSERT_TRUE(c2_Info->rf == 1);
+  retain(c2);
+  CU_ASSERT_TRUE(c2_Info->rf == 2);
+  release(c2);
+  CU_ASSERT_TRUE(c2_Info->rf == 1);
+  retain(c1);
+  release(c1);
+  CU_ASSERT_TRUE(c1_Info->rf == 1);
+  
+  c1->cell = allocate(sizeof(cell1_t), cell_destructor1);
+  c2->cell = allocate(sizeof(cell2_t), cell_destructor2);
+  objectInfo_t *c1_nextInfo = ((obj *) c1->cell - sizeof(objectInfo_t));
+  objectInfo_t *c2_nextInfo = ((obj *) c2->cell - sizeof(objectInfo_t));
+
+  
+  CU_ASSERT_TRUE(c1_nextInfo ->rf == 0);
+  retain(c1->cell);
+  CU_ASSERT_TRUE(c1_nextInfo ->rf == 1);
+  CU_ASSERT_TRUE(c2_nextInfo ->rf == 0);
+  retain(c2->cell);
+  CU_ASSERT_TRUE(c2_nextInfo ->rf == 1);
+  CU_ASSERT_TRUE(c2_Info->rf ==1);
+  release(c2->cell);
+  CU_ASSERT_TRUE(c2_Info->rf ==1);
+  CU_ASSERT_TRUE(c1_Info->rf==1);
+  retain(c2->cell);
+  CU_ASSERT_TRUE(c2_nextInfo ->rf == 1);
+
+  //cell1_t *c1next = c1->cell;
+  c1->cell->cell = allocate(sizeof(cell1_t), cell_destructor1);
+
+   c2->cell->cell = allocate(sizeof(cell1_t), cell_destructor2);
+  objectInfo_t *c1_nextNextInfo = ((obj *) c1->cell->cell - sizeof(objectInfo_t));
+   objectInfo_t *c2_nextNextInfo = ((obj *) c2->cell->cell - sizeof(objectInfo_t));
+  CU_ASSERT_TRUE(c1_nextNextInfo ->rf == 0);
+  retain(c1->cell->cell);
+  CU_ASSERT_TRUE(c1_nextNextInfo ->rf == 1);
+  retain(c1->cell->cell);
+  CU_ASSERT_TRUE(c1_nextNextInfo ->rf == 2);
+  retain(c1->cell->cell);
+  CU_ASSERT_TRUE(c1_nextNextInfo ->rf == 3);
+  CU_ASSERT_TRUE(c2_nextNextInfo ->rf == 0);
+  release(c2->cell->cell);
+
+  c1->cell->cell = NULL;
+  
+
+  deallocate(c1);
+  //deallocate(c2);
  
 }
 void test_release()
 {
+    cell1_t *c1 = allocate(sizeof(cell1_t), cell_destructor1);
+  cell2_t *c2 = allocate(sizeof(cell2_t), cell_destructor2);
+  objectInfo_t *c1_Info = ((obj *) c1 - sizeof(objectInfo_t));
+  objectInfo_t *c2_Info = ((obj *) c2 - sizeof(objectInfo_t));
 
+  CU_ASSERT_TRUE(c1_Info->rf == 0);
+  retain(c1);
+  CU_ASSERT_TRUE(c1_Info->rf == 1);
+  CU_ASSERT_TRUE(c2_Info->rf == 0);
+  retain(c2);
+  CU_ASSERT_TRUE(c2_Info->rf == 1);
+  retain(c2);
+  CU_ASSERT_TRUE(c2_Info->rf == 2);
+
+  
+  c1->cell = allocate(sizeof(cell1_t), cell_destructor1);
+  c2->cell = allocate(sizeof(cell2_t), cell_destructor2);
+  objectInfo_t *c1_nextInfo = ((obj *) c1->cell - sizeof(objectInfo_t));
+  objectInfo_t *c2_nextInfo = ((obj *) c2->cell - sizeof(objectInfo_t));
+  
+  CU_ASSERT_TRUE(c1_nextInfo ->rf == 0);
+  retain(c1->cell);
+  CU_ASSERT_TRUE(c1_nextInfo ->rf == 1);
+  CU_ASSERT_TRUE(c2_nextInfo ->rf == 0);
+  retain(c2->cell);
+  CU_ASSERT_TRUE(c2_nextInfo ->rf == 1);
+  CU_ASSERT_TRUE(c2_Info->rf ==2);
+  CU_ASSERT_TRUE(c1_Info->rf==1);
+  retain(c2->cell);
+  CU_ASSERT_TRUE(c2_nextInfo ->rf == 2);
+
+  //cell1_t *c1next = c1->cell;
+  c1->cell->cell = allocate(sizeof(cell1_t), cell_destructor1);
+  objectInfo_t *c1_nextNextInfo = ((obj *) c1->cell->cell - sizeof(objectInfo_t));
+  CU_ASSERT_TRUE(c1_nextNextInfo ->rf == 0);
+  retain(c1->cell->cell);
+  CU_ASSERT_TRUE(c1_nextNextInfo ->rf == 1);
+  retain(c1->cell->cell);
+  CU_ASSERT_TRUE(c1_nextNextInfo ->rf == 2);
+  retain(c1->cell->cell);
+  CU_ASSERT_TRUE(c1_nextNextInfo ->rf == 3);
+
+  c1->cell->cell = NULL;
+  
+
+  deallocate(c1);
+  deallocate(c2);
+  
 }
 void test_rc()
 {
